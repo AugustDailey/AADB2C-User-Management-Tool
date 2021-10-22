aecff638c095b6b594498f0c83bc1867d76ab71f
August Dailey <37079788+AugustDailey@users.noreply.github.com>
2021-10-22 18:12:45 -0500

    Initialize Project (C# project, CI/CD, and gitignore) (#1)
    * Add gitignore
    
    * Add ci/cd pipeline config
    
    * Initialize project

---
+++ UserManagementTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserManagementTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
