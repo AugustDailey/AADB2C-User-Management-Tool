0c99c2b2ec6b384c768dc277957ae42dfef92c1f
August Dailey <daileyab@rose-hulman.edu>
2021-10-24 15:49:16 -0500

    Add dependency injection logic

---
+++ UserManagementTool/App/Application.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserManagementTool.App
+{
+    public class Application : IApplication
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UserManagementTool/App/IApplication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserManagementTool.App
+{
+    public interface IApplication
+    {
+        void Run();
+    }
+}
