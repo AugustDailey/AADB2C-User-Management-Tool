4359e33f2c67f86fbfe59aff2f34fd13f34b758a
August Dailey <daileyab@rose-hulman.edu>
2021-11-02 19:47:49 -0500

    Remove unnecessary usings

--- UserManagementTool/App/IApplication.cs
+++ UserManagementTool/App/IApplication.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace UserManagementTool.App
+﻿namespace UserManagementTool.App
 {
     public interface IApplication
     {
